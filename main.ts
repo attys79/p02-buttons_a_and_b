a2810474c2b051c2019e4ce067564de92f71855c
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-07 12:51:46 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.AB)) {
+        basic.showString("X")
+    } else if (input.buttonIsPressed(Button.A)) {
+        basic.showString("A")
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showString("B")
+    } else {
+        basic.clearScreen()
+    }
 })
