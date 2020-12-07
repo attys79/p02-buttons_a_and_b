5bfc7bad6de8a13aa642a8515a0532546d7d98b9
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-07 12:55:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    basic.pause(delay)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("X")
+    basic.pause(delay)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    basic.pause(delay)
+    basic.clearScreen()
+})
+let delay = 0
+delay = 200
 basic.forever(function () {
 	
 })
